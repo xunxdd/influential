0582ec567974a68a690076b7bd907bb7e79bdf3a
xun.ding <xun.ding@cision.com>
2015-10-23 14:12:36 -0500

    delete all sorts of junk

--- Influencial/Global.asax.cs
+++ Influencial/Global.asax.cs
@@ -11,7 +11,7 @@ namespace Influencial
     {
         protected void Application_Start()
         {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
+           
         }
     }
 }
